3a18333ced0f2896a5c02a9876e75e8b804a707b
KayqNobre <kayquerodrigues@alu.ufc.br>
2026-01-22 19:45:44 -0300

    organização do código e remoção do cors

--- server/index.js
+++ server/index.js
@@ -1,10 +1,7 @@
 import express from 'express';
-import cors from 'cors';
-
 const app = express();
 const port = 3000;
 app.use(express.json());
-app.use(cors());
 
 // ================================
 // Persistência em memória
